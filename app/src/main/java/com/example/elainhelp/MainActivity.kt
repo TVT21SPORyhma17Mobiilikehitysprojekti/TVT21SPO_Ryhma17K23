bd16ce302f99ec4fabac480f57f537bfd7db5ffe
Miifu <mil_la2000@hotmail.com>
2023-04-07 16:00:42 +0300

    Marsusivua eteenpäin ja yritys saada imageButton toimimaan

--- app/src/main/java/com/example/elainhelp/MainActivity.kt
+++ app/src/main/java/com/example/elainhelp/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.elainhelp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
+        val guineapig = findViewById<ImageButton>(R.id.guineapigIB)
+        val dog = findViewById<ImageButton>(R.id.dogIB)
+        val cat = findViewById<ImageButton>(R.id.catIB)
 
+    }
+    fun onClick(view: View?) {
+        val intent = Intent(this, activity_guineapig::class.java)
+    startActivity(intent);
+    }
 }
