6158be7bead2980c781ecb5b404a3053939ed630
Tumeek <99178421+Tumeek@users.noreply.github.com>
2023-04-18 18:36:20 +0300

    Enemmän projektia eteenpäin

---
+++ app/src/main/java/com/example/elainhelp/activity_CatTeethClaws.kt
@@ -0,0 +1,11 @@
+package com.example.elainhelp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_CatTeethClaws : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cat_teeth_claws)
+    }
+}
