564eb36c55d3cf64b987ec9b536f05c9deb1262c
Tumeek <99178421+Tumeek@users.noreply.github.com>
2023-04-18 17:31:25 +0300

    Enemmän projektia eteenpäin

--- app/src/main/java/com/example/elainhelp/activity_cat.kt
+++ app/src/main/java/com/example/elainhelp/activity_cat.kt
@@ -1,11 +1,36 @@
 package com.example.elainhelp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class activity_cat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cat)
+
+
+        val foodCatBtn = findViewById<Button>(R.id.buFeedingCat)
+        val teethCatBtn = findViewById<Button>(R.id.buTeethCat)
+        val socialCatBtn = findViewById<Button>(R.id.buSocialCat)
+
+
+        foodCatBtn.setOnClickListener {
+            val intent = Intent(this, activity_CatFood::class.java)
+            startActivity(intent)
+
+        }
+        teethCatBtn.setOnClickListener {
+            val intent = Intent(this, activity_CatTeethClaws::class.java)
+            startActivity(intent)
+
+        }
+        socialCatBtn.setOnClickListener {
+            val intent = Intent(this, activity_CatSocial::class.java)
+            startActivity(intent)
+
+        }
     }
+
 }
